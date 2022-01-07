c086ce8a50bee6aff3d38d0ecbc5db84ec90c004
skyleaworlder <870033938@qq.com>
2022-01-08 05:58:38 +0800

    feat: 增加了对路径后缀的匹配，主要针对曾经的 ApiDocs.bp，目前默认为 Android.bp

--- services/filter.py
+++ services/filter.py
@@ -4,20 +4,33 @@ from typing import List
 
 class NinjaModuleFilter:
     @classmethod
-    def Defined(cls, content: List[str], path_prefix="frameworks/") -> bool:
+    def Defined(cls, content: List[str],
+                path_prefix="frameworks/", path_suffix="Android.bp") -> bool:
         multi_line_defined = False
         for line in content:
             # 如果这个 define 是多行定义的，同时也以前缀开头，
             # 那么就应该 return True
             if multi_line_defined and line.startswith(f"# {path_prefix}"):
-                return True
+                parts = line.split(":")
+                path = parts[0]
+                if path.startswith(f"# {path_prefix}") and path.endswith(path_suffix):
+                    return True
 
             # 在检查多行之后，再判断是否是以 Defined 开头的单行
             if not line.startswith("# Defined:"):
                 continue
 
-            if line.startswith(f"# Defined: {path_prefix}"):
-                return True
-            elif line.startswith("# Defined:\n"):
+            # 如果以 "# Defined:\n" 开头 / 结尾，说明要开启一个多行
+            if line.startswith("# Defined:\n"):
                 multi_line_defined = True
+                continue
+
+            parts = [part.strip() for part in line.split(":")]
+            defined = parts[0]
+            path = parts[1]
+            #print(defined, path, path.startswith(path_prefix), path.endswith(path_suffix))
+            if defined == "# Defined" \
+                and path.startswith(path_prefix) \
+                and path.endswith(path_suffix):
+                return True
         return False
